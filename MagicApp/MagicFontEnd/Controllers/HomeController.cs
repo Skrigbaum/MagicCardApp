9b610a0bdd5c66b1bd41c5e9e2021c683b55ed1e
Spencer Krigbaum <skrigbaum872002@gmail.com>
2016-08-31 10:02:16 -0400

    typos
    Fixed Typos and Changed some of the calls to better reflect the roles

--- MagicApp/MagicFontEnd/Controllers/HomeController.cs
+++ MagicApp/MagicFontEnd/Controllers/HomeController.cs
@@ -30,11 +30,11 @@ namespace MagicFontEnd.Controllers
        
        public string Creature()
         {
-           var creature = MagicApp.Controllers.CardController.RandomCreature();
+           var creature = MagicApp.Controllers.CardController.RandomCreatureorEnchantment();
            var ImgUrl = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=" + creature.MultiverseId + "&type=card";
-            if (System.IO.File.Exists("C:\\Users\\skrigbaum\\MagicCardApp\\MagicApp\\MagicFontEnd\\Images\\" + creature.Name + ".full.jpg"))
+            if (System.IO.File.Exists(@"C:\Users\skrigbaum\MagicCardApp\MagicApp\MagicFontEnd\Images\" + creature.Name + ".full.jpg"))
             {
-                ImgUrl = "\\Images\\" + creature.Name + ".full.jpg";
+                ImgUrl = @"\Images\" + creature.Name + ".full.jpg";
                 return ImgUrl;
             }
             else
@@ -52,9 +52,9 @@ namespace MagicFontEnd.Controllers
             var Land = MagicApp.Controllers.CardController.RandomLand();
             var ImgUrl = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=" + Land.MultiverseId + "&type=card";
 
-            if (System.IO.File.Exists("C:\\Users\\skrigbaum\\MagicCardApp\\MagicApp\\MagicFontEnd\\Images\\"+ Land.Name + ".full.jpg"))
+            if (System.IO.File.Exists(@"C:\Users\skrigbaum\MagicCardApp\MagicApp\MagicFontEnd\Images\" + Land.Name + ".full.jpg"))
             {
-                ImgUrl =  "\\Images\\" + Land.Name +".full.jpg";
+                ImgUrl =  @"\Images\" + Land.Name +".full.jpg";
                 return ImgUrl;
             }
             else
@@ -68,11 +68,11 @@ namespace MagicFontEnd.Controllers
 
         public string Helper()
         {
-            var helper = MagicApp.Controllers.CardController.RandomCreatureorEnchantment();
+            var helper = MagicApp.Controllers.CardController.RandomCreature();
             var ImgUrl = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=" + helper.MultiverseId + "&type=card";
-            if (System.IO.File.Exists("C:\\Users\\skrigbaum\\MagicCardApp\\MagicApp\\MagicFontEnd\\Images\\" + helper.Name + ".full.jpg"))
+            if (System.IO.File.Exists(@"C:\Users\skrigbaum\MagicCardApp\MagicApp\MagicFontEnd\Images\" + helper.Name + ".full.jpg"))
             {
-                ImgUrl = "\\Images\\" + helper.Name + ".full.jpg";
+                ImgUrl = @"\Images\" + helper.Name + ".full.jpg";
                 return ImgUrl;
             }
             else
@@ -89,9 +89,9 @@ namespace MagicFontEnd.Controllers
         {
             var solution = MagicApp.Controllers.CardController.RandomSorceryorArtifact();
             var ImgUrl = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=" + solution.MultiverseId + "&type=card";
-            if (System.IO.File.Exists("C:\\Users\\skrigbaum\\MagicCardApp\\MagicApp\\MagicFontEnd\\Images\\" + solution.Name + ".full.jpg"))
+            if (System.IO.File.Exists(@"C:\Users\skrigbaum\MagicCardApp\MagicApp\MagicFontEnd\Images\" + solution.Name + ".full.jpg"))
             {
-                ImgUrl = "\\Images\\" + solution.Name + ".full.jpg";
+                ImgUrl = @"\Images\" + solution.Name + ".full.jpg";
                 return ImgUrl;
             }
             else
